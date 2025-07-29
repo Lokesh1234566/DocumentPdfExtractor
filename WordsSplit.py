fea4dfd9b8b582671bec1251377f85ce6dd0ccd7
Lokesh R <lokesh21031999@gmail.com>
2025-07-29 11:35:01 +0530

    doclingparse

--- WordsSplit.py
+++ WordsSplit.py
@@ -13,9 +13,12 @@ try:
             elif line_count == 1:
                 my_list1 = words
             line_count += 1
-
-    print("my_list:", my_list)
-    print("my_list1:", my_list1)
+    
+    # Convert the two lists to a dictionary
+    my_dict = dict(zip(my_list, my_list1))
+    # print("my_list:", my_list)
+    # print("my_list1:", my_list1)
+    print(my_dict)
 
 except FileNotFoundError:
     print(f"Error: The file '{file_path}' was not found.")
---
+++ doclingparsepdf.py
@@ -0,0 +1,19 @@
+from docling_core.types.doc.page import TextCellUnit
+from docling_parse.pdf_parser import DoclingPdfParser, PdfDocument
+
+parser = DoclingPdfParser()
+
+pdf_doc: PdfDocument = parser.load(
+    path_or_stream="./pdf/A.pdf"
+)
+
+# PdfDocument.iterate_pages() will automatically populate pages as they are yielded.
+for page_no, pred_page in pdf_doc.iterate_pages():
+
+    # iterate over the word-cells
+    for word in pred_page.iterate_cells(unit_type=TextCellUnit.WORD):
+        print(word.rect, ": ", word.text)
+
+        # create a PIL image with the char cells
+    img = pred_page.render_as_image(cell_unit=TextCellUnit.CHAR)
+    img.show()
