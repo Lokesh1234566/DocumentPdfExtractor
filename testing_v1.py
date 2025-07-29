78dd32237925976afb88ea7eb6358b3157e97c8b
Lokesh R <lokesh21031999@gmail.com>
2025-07-29 15:20:54 +0530

    page count file testing_v1.py file

---
+++ testing_v1.py
@@ -0,0 +1,14 @@
+import pdfplumber
+
+def extract_text_with_layout(pdf_path):
+    full_text = ""
+    with pdfplumber.open(pdf_path) as pdf:
+        page_count = len(pdf.pages)
+        print(f"Total number of pages: {page_count}\n")
+        for page in pdf.pages:
+            full_text += page.extract_text(layout=True) + "\n"
+    return full_text
+
+# Usage
+text = extract_text_with_layout("./pdf/A.pdf")
+print(text)
